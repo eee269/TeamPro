a766cfd4603e67225bd363e8fdd7ae8c74fb3137
eee269 <skfkd3123@gmail.com>
2020-11-18 10:30:50 +0900

    20.11.18.yj_srctest

--- Itwillbs_8/src/action/Action.java
+++ Itwillbs_8/src/action/Action.java
@@ -1,4 +1,4 @@
-package board.action;
+package action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
---
+++ Itwillbs_8/src/vo/ActionForward.java
@@ -0,0 +1,22 @@
+package vo;
+
+public class ActionForward {
+	private String path; // 포워딩 할 View 페이지 URL 저장
+	private boolean redirect; // 포워딩 방식 저장
+	
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public boolean isRedirect() {
+		return redirect;
+	}
+	public void setRedirect(boolean redirect) {
+		this.redirect = redirect;
+	}
+	
+	
+	
+}
