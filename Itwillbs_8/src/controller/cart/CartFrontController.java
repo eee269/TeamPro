aedeb186c9f6460d138f63c13c6ba02f2482d2d5
unaa <zzaaqqqaz@naver.com>
2020-11-20 09:18:40 +0900

    201120_un_패키지추가

---
+++ Itwillbs_8/src/controller/cart/CartFrontController.java
@@ -0,0 +1,38 @@
+package controller.cart;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import action.Action;
+import vo.ActionForward;
+
+
+@WebServlet("*.po")
+public class CartFrontController extends HttpServlet {
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// POST 요청에 한글 처리
+		request.setCharacterEncoding("UTF-8");
+		
+		// 서블릿 주소 가져오기
+		String command = request.getServletPath();
+		
+		// 필요한 변수 선언
+		Action action = null;
+		ActionForward forward = null;
+	}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			doProcess(request, response);
+	}
+
+}
