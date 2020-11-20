c2285f1d38933762631ad1b561323187d7a00e4f
eee269 <skfkd3123@gmail.com>
2020-11-20 09:21:54 +0900

    20.11.20.yj

---
+++ Itwillbs_8/src/vo/ActionForward.java
@@ -0,0 +1,20 @@
+package vo;
+
+public class ActionForward {
+	private String path;
+	private boolean redirect;
+	
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public boolean isRedirect() {
+		return redirect;
+	}
+	public void setRedirect(boolean redirect) {
+		this.redirect = redirect;
+	}
+
+}
---
+++ Itwillbs_8/src/vo/PageInfo.java
@@ -0,0 +1,50 @@
+package vo;
+
+public class PageInfo {
+	private int page;
+	private int maxPage;
+	private int startPage;
+	private int endPage;
+	private int listCount;
+	
+	public PageInfo() {}
+	
+	public PageInfo(int page, int maxPage, int startPage, int endPage, int listCount) {
+		super();
+		this.page = page;
+		this.maxPage = maxPage;
+		this.startPage = startPage;
+		this.endPage = endPage;
+		this.listCount = listCount;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getMaxPage() {
+		return maxPage;
+	}
+	public void setMaxPage(int maxPage) {
+		this.maxPage = maxPage;
+	}
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+	public int getListCount() {
+		return listCount;
+	}
+	public void setListCount(int listCount) {
+		this.listCount = listCount;
+	}
+}
