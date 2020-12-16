53a461e48e29a0710b1a3699ce5ad2d205170191
sk8er <sk8er@DESKTOP-27LUPNB>
2020-12-16 18:19:36 +0900

    201216_BIN_[add] 상품리뷰답글 추가

--- Itwillbs_8/src/svc/product/review/ProdReviewWriteService.java
+++ Itwillbs_8/src/svc/product/review/ProdReviewWriteService.java
@@ -65,5 +65,32 @@ public class ProdReviewWriteService {
 		return isReviewSuccess;
 	}
 	// --------------modifyReview()------------------
+	// --------------registReviewReply()------------------
+	public boolean registReviewReply(ProdReviewBean prodReviewBean) {
+		System.out.println("ProdReviewWriteService");
+		// 리뷰 등록
+		boolean isReviewSuccess = false;
+		
+		Connection con = getConnection();
+		
+		ProdReviewDAO prodReviewDAO = ProdReviewDAO.getInstance();
+		
+		prodReviewDAO.setConnection(con);
+		
+		int insertCount = prodReviewDAO.insertReviewReply(prodReviewBean);
+		
+		if(insertCount > 0) {
+			// 리뷰 등록 성공
+			commit(con);
+			isReviewSuccess = true;
+		}else {
+			rollback(con);
+		}
+		
+		close(con);
+		
+		return isReviewSuccess;
+	}
+	// --------------registReviewReply()------------------
 
 }
