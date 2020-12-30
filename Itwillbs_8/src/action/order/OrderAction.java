416170129cb19964864ec02a67177317bdb51275
GoUNaa <zzaaqqqaz@naver.com>
2020-12-30 09:01:47 +0900

    20201230 땡기지마시오

--- Itwillbs_8/src/action/order/OrderAction.java
+++ Itwillbs_8/src/action/order/OrderAction.java
@@ -52,7 +52,7 @@ public class OrderAction implements Action {
 			System.out.println("Action List : "+cartList.get(i).getProduct_basicCode());
 			System.out.println("Action List : "+cartList.get(i).getOpt_productCode());
 		}
-
+		
 		forward = new ActionForward();
 		forward.setPath("/order/order.jsp");
 		request.setAttribute("cartList", cartList);
